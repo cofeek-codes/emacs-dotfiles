0d5646906c64fd67a83ba90f93e7925735866681
cofeek-codes <kormyshev11@mail.ru>
2023-09-09 01:13:51 +0500

    ido-mode colors fixed

--- init.el
+++ init.el
@@ -518,4 +518,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ido-first-match ((t (:foreground "#FFDD33" :weight bold))))
+ '(ido-incomplete-regexp ((t (:foreground "white"))))
+ '(ido-only-match ((t (:foreground "#FF9B21"))))
+ '(ido-subdir ((t (:foreground "#949bb0" :weight bold)))))
