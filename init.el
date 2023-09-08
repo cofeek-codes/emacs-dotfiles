2464d983acbcdc64618cc29c92da5d04209a0ee0
cofeek-codes <kormyshev11@mail.ru>
2023-09-09 00:51:15 +0500

    ido mode added

--- init.el
+++ init.el
@@ -132,7 +132,7 @@
  ;; (package-refresh-contents) don't want it to refresh every time
 
 
-(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp hl-line  projectile hydra flycheck company avy which-key helm-xref dap-mode json-mode move-text lsp-ui sideline sideline-lsp clang-format emmet-mode resize-window magit prettier-js typescript-mode rust-mode go-mode php-mode telega lsp-pyright py-autopep8 wakatime-mode company-tabnine csharp-mode haskell-mode lsp-haskell cargo quelpa quelpa-use-package))
+(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp hl-line  projectile hydra flycheck company avy which-key helm-xref dap-mode json-mode move-text lsp-ui sideline sideline-lsp clang-format emmet-mode resize-window magit prettier-js typescript-mode rust-mode go-mode php-mode telega lsp-pyright py-autopep8 wakatime-mode company-tabnine csharp-mode haskell-mode lsp-haskell cargo quelpa quelpa-use-package ido-completing-read+ smex))
  
 
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
@@ -482,8 +482,19 @@
 ;; minibuffer
 
 
+(require 'ido-completing-read+)
+(require 'smex)
 
 
+(setq ido-enable-flex-matching t)
+(ido-everywhere t)
+(ido-mode 1)
+(ido-ubiquitous-mode 1)
+
+
+(setq ido-use-filename-at-point 'guess)
+
+(setq ido-create-new-buffer 'always)
 
 
 
