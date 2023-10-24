c60b7cb4e90c2880f50bad811a4cd51df48a8318
cofeek-codes <kormyshev11@mail.ru>
2023-10-25 01:27:27 +0500

    multiple cursrors block updated

--- init.el
+++ init.el
@@ -622,11 +622,11 @@
 
 (require 'multiple-cursors)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C-c C->")         'mc/mark-next-like-this)
-(global-set-key (kbd "C-c C-<")         'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C->")     'mc/mark-next-like-this)
+(global-set-key (kbd "C-c C-<")     'mc/mark-previous-like-this)
 (global-set-key (kbd "C-\"")        'mc/skip-to-next-like-this)
 (global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this)
-;; (global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
+(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
 
 
 ;; NeoTree
