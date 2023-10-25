6524adae25913e9096adf44c6288cb2783812c4a
cofeek-codes <kormyshev11@mail.ru>
2023-10-25 16:15:48 +0500

    place cursor on alt-click implemented

--- init.el
+++ init.el
@@ -621,12 +621,14 @@
 ;; Multiple Cursors
 
 (require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C-c C->")     'mc/mark-next-like-this)
-(global-set-key (kbd "C-c C-<")     'mc/mark-previous-like-this)
-(global-set-key (kbd "C-\"")        'mc/skip-to-next-like-this)
-(global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this)
-(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
+(global-set-key (kbd "C-S-c C-S-c")      'mc/edit-lines)
+(global-set-key (kbd "C-c C->")          'mc/mark-next-like-this)
+(global-set-key (kbd "C-c C-<")          'mc/mark-previous-like-this)
+(global-set-key (kbd "C-\"")             'mc/skip-to-next-like-this)
+(global-set-key (kbd "C-:")              'mc/skip-to-previous-like-this)
+(global-set-key (kbd "M-<down-mouse-1>") 'mc/add-cursor-on-click)
+
+
 
 
 ;; NeoTree
