6e204abd09e10e7f46405cb37c64cb94795ef88d
cofeek-codes <kormyshev11@mail.ru>
2023-12-13 15:39:09 +0500

    plantuml support added

--- init.el
+++ init.el
@@ -160,7 +160,7 @@
  ;; (package-refresh-contents) don't want it to refresh every time
 
 
-(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp hl-line  projectile hydra flycheck company avy which-key helm-xref dap-mode json-mode move-text lsp-ui sideline sideline-lsp clang-format emmet-mode resize-window magit prettier-js typescript-mode rust-mode go-mode php-mode telega lsp-pyright py-autopep8 wakatime-mode company-tabnine csharp-mode haskell-mode lsp-haskell cargo quelpa quelpa-use-package ido-completing-read+ smex projectile ag multiple-cursors neotree all-the-icons lsp-pascal yaml-mode nasm-mode lua-mode company-lua company-web python-mode company-math elixir-mode))
+(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp hl-line  projectile hydra flycheck company avy which-key helm-xref dap-mode json-mode move-text lsp-ui sideline sideline-lsp clang-format emmet-mode resize-window magit prettier-js typescript-mode rust-mode go-mode php-mode telega lsp-pyright py-autopep8 wakatime-mode company-tabnine csharp-mode haskell-mode lsp-haskell cargo quelpa quelpa-use-package ido-completing-read+ smex projectile ag multiple-cursors neotree all-the-icons lsp-pascal yaml-mode nasm-mode lua-mode company-lua company-web python-mode company-math elixir-mode plantuml-mode flycheck-plantuml))
  
 
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
@@ -234,7 +234,7 @@
 
 (add-hook 'before-save-hook 'clang-format-on-save)
 
-;; end C/C++=========================
+;; end C/C++ =========================
 
 
 ;; Web
@@ -601,13 +601,6 @@
 
 
 
-;; tabnine
-
-;; (require 'company-tabnine)
-
-;; (add-to-list 'company-backends #'company-tabnine)
-
-;; Trigger completion immediately.
 (setq company-idle-delay 0)
 
 ;; Number the candidates (use M-1, M-2 etc to select completions).
@@ -731,6 +724,21 @@
 (add-to-list 'company-backends 'company-math-symbols-unicode)
 (add-to-list 'company-backends 'company-math-symbols-latex)
 
+;; Latex end ==============
+
+
+;; UML
+
+(setq plantuml-jar-path "/usr/bin/plantuml.jar")
+(setq plantuml-default-exec-mode 'jar)
+
+(add-to-list 'auto-mode-alist '("\\.uml\\'" . plantuml-mode))
+
+(with-eval-after-load 'flycheck
+  (require 'flycheck-plantuml)
+  (flycheck-plantuml-setup))
+
+;; UML end ==============
 
 
 
