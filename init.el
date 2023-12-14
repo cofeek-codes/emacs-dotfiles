81e5b4d060118dceb16d4b674152f1cdfe47e0af
cofeek-codes <kormyshev11@mail.ru>
2023-12-14 20:44:47 +0500

    moved css-selector highlight to init file from theme file

--- init.el
+++ init.el
@@ -778,6 +778,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(css-selector ((t (:inherit font-lock-type-face))))
  '(font-latex-script-char-face ((t (:foreground "Green"))))
  '(font-latex-sectioning-5-face ((t (:inherit variable-pitch :foreground "#949bb0" :weight bold))))
  '(ido-first-match ((t (:foreground "#FFDD33" :weight bold))))
--- tsoding-theme.el
+++ tsoding-theme.el
@@ -30,7 +30,6 @@
  '(region ((t (:extend nil :background "#504945"))))
  '(mode-line-inactive ((t (:inherit mode-line :background "#252525" :foreground "#f8f8f8" :box (:line-width (1 . -1) :color "#252525")))))
  '(font-lock-constant-face ((t (:inherit font-lock-type-face))))
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#181818" :foreground "#FFF" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 181 :width normal :foundry "CTDB" :family "Fira Code"))))
- '(css-selector ((t (:inherit font-lock-type-face)))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#181818" :foreground "#FFF" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 181 :width normal :foundry "CTDB" :family "Fira Code")))))
 
 (provide-theme 'tsoding)
