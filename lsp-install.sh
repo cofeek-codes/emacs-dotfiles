2a7f19058f479eeabe184056bcfb54545b489010
cofeek-codes <kormyshev11@mail.ru>
2024-01-14 22:59:19 +0500

    shell script code organization and update

--- lsp-install.sh
+++ lsp-install.sh
@@ -2,10 +2,22 @@
 
 set -xe
 
+# ================== node lsps ==================
+echo "================== node lsps =================="
+
 npm i -g @prisma/language-server &&
 npm i -g pyright &&
 npm i -g typescript-language-server &&
 npm i -g intelephense &&
 sudo npm i -g livedown
+
+# ================== cargo lsps ==================
+echo "================== cargo lsps =================="
+
 cargo install --git https://github.com/latex-lsp/texlab --tag v5.12.1 &&
 cargo install slint-lsp
+
+# ================== other stuff ==================
+echo "================== other stuff =================="
+
+pip install pygments 
