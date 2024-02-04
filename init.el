16bb997df6773a80a53bfef9bef84df265736e9d
cofeek-codes <kormyshev11@mail.ru>
2024-02-05 00:48:04 +0500

    expand-region addded

--- init.el
+++ init.el
@@ -16,6 +16,8 @@
 (global-set-key (kbd "C-a") 'mark-whole-buffer)
 (global-set-key (kbd "C-e") 'move-end-of-line)
 (global-set-key (kbd "C-S-f") 'query-replace)
+(global-set-key (kbd "C-{") 'er/contract-region)
+(global-set-key (kbd "C-}") 'er/expand-region)
 
 
 ;; tabs
@@ -185,7 +187,7 @@ With prefix arg N, delete backward to the start of the Nth word."
  ;; (package-refresh-contents) don't want it to refresh every time
 
 
-(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp hl-line  projectile hydra flycheck company avy which-key helm-xref dap-mode json-mode move-text lsp-ui sideline sideline-lsp clang-format emmet-mode resize-window magit prettier-js typescript-mode rust-mode go-mode php-mode telega lsp-pyright py-autopep8 wakatime-mode company-tabnine csharp-mode haskell-mode lsp-haskell cargo quelpa quelpa-use-package ido-completing-read+ smex projectile ag multiple-cursors neotree all-the-icons lsp-pascal yaml-mode nasm-mode lua-mode company-lua company-web python-mode company-math elixir-mode seq transpose-frame slint-mode zenburn-theme dune-format))
+(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp hl-line  projectile hydra flycheck company avy which-key helm-xref dap-mode json-mode move-text lsp-ui sideline sideline-lsp clang-format emmet-mode resize-window magit prettier-js typescript-mode rust-mode go-mode php-mode telega lsp-pyright py-autopep8 wakatime-mode company-tabnine csharp-mode haskell-mode lsp-haskell cargo quelpa quelpa-use-package ido-completing-read+ smex projectile ag multiple-cursors neotree all-the-icons lsp-pascal yaml-mode nasm-mode lua-mode company-lua company-web python-mode company-math elixir-mode seq transpose-frame slint-mode zenburn-theme dune-format expand-region))
  
 
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
