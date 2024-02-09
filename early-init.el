c2b25cbfcebefdf76951778b4a5dbbdb208e7252
cofeek-codes <kormyshev11@mail.ru>
2024-02-09 22:36:43 +0500

    big optimizations applied

--- early-init.el
+++ early-init.el
@@ -2,3 +2,5 @@
 
 (setq inhibit-startup-screen t)
 
+(setq gc-cons-threshold (*  50  1000  1000))
+(add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold (*  2  1000  1000))))
--- init.el
+++ init.el
@@ -313,17 +313,6 @@ With prefix arg N, delete backward to the start of the Nth word."
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
 
-
-;; (setq gc-cons-threshold (* 100 1024 1024)
-;;      read-process-output-max (* 1024 1024)
-;;    treemacs-space-between-root-nodes nil
-;;  company-idle-delay 0.0
-;; company-minimum-prefix-length 1
-;; lsp-idle-delay 0.1)
-
-
-
-
 ;; format on save
 (setq clang-format-style "file")
 (setq clang-format-fallback-style "llvm")
