8c3fe39baa871695dc19bd80190728cd309eed94
cofeek-codes <kormyshev11@mail.ru>
2024-02-15 12:20:21 +0500

    scrollbar disabled

--- early-init.el
+++ early-init.el
@@ -2,5 +2,7 @@
 
 (setq inhibit-startup-screen t)
 
+(scroll-bar-mode 0)
+
 (setq gc-cons-threshold (*  50  1000  1000))
 (add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold (*  2  1000  1000))))
