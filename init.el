d8b6070b6037b11df83a76cff3b689c46baab5b8
cofeek-codes <kormyshev11@mail.ru>
2024-04-18 22:51:30 +0500

    mc-mode always run for all

--- init.el
+++ init.el
@@ -948,6 +948,7 @@ With prefix arg N, delete backward to the start of the Nth word."
  '(custom-safe-themes
    '("0d747b714779e0148e602581746b8806926724a72fb247129c7359f5f6ad80c2" "18cf5d20a45ea1dff2e2ffd6fbcd15082f9aa9705011a3929e77129a971d1cb3" default))
  '(doc-view-continuous t)
+ '(mc/always-run-for-all t)
  '(org-support-shift-select 'always)
  '(plantuml-indent-level 3)
  '(plantuml-suppress-deprecation-warning nil)
