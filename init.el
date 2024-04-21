cfaf1fab591c9f076c28f4bbc2a3459ae3a529bc
cofeek-codes <kormyshev11@mail.ru>
2024-04-21 18:04:58 +0500

    dotenv-mode added

--- init.el
+++ init.el
@@ -47,6 +47,7 @@
 (straight-use-package 'dap-mode)
 (straight-use-package 'json-mode)
 (straight-use-package 'web-mode)
+(straight-use-package 'dotenv-mode)
 (straight-use-package 'move-text)
 (straight-use-package 'wrap-region)
 (straight-use-package 'clang-format)
