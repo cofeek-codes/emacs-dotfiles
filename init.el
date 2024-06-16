e36514644c5e56bf0e56463d261cf2eb21f87f7b
cofeek-codes <kormyshev11@mail.ru>
2024-06-17 00:37:32 +0500

    org-roam template set

--- init.el
+++ init.el
@@ -932,8 +932,12 @@
          ;; Dailies
          ("C-c n j" . org-roam-dailies-capture-today))
   :config
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (setq org-roam-capture-templates
+        '(("d" "default" plain
+           "%?"
+           :if-new (file+head "${title}.org" "#+title: ${title}\n")
+           :unarrowed t))
+        )
   (org-roam-db-autosync-mode)
   ;; If using org-roam-protocol
   (require 'org-roam-protocol))
