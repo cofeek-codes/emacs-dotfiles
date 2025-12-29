3a8ea77f585bdb50d5233cb69a70454c446f0964
Cofeek <kormyshev11@mail.ru>
2025-12-29 20:40:04 +0500

    made hsplit vertical border less visible

--- init.el
+++ init.el
@@ -82,4 +82,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(vertical-border ((t (:background "#181818" :distant-foreground "#181818" :foreground "#181818")))))
