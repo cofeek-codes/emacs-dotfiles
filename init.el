e66fb2438f149c0fadbfb5fd37a4c94d9cafd4ad
Cofeek <kormyshev11@mail.ru>
2025-12-29 20:18:44 +0500

    new binds and packages

--- init.el
+++ init.el
@@ -11,6 +11,10 @@
 (global-set-key (kbd "C-k") 'kill-whole-line)
 (global-set-key (kbd "C-p") 'company-complete)
 (global-set-key (kbd "C-/") 'comment-line)
+(global-set-key (kbd "C-c ;") 'windresize)
+(global-set-key (kbd "C-c C-c") 'compile)
+
+
 
 (windmove-default-keybindings)
 
@@ -68,7 +72,8 @@
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("nongnu" . "https://elpa.nongnu.org/nongnu/")
      ("melpa" . "https://melpa.org/packages/")))
- '(package-selected-packages '(company magit gruber-darker-theme))
+ '(package-selected-packages '(windresize company magit gruber-darker-theme))
+ '(ring-bell-function 'ignore)
  '(scroll-bar-mode nil)
  '(tab-width 4)
  '(tool-bar-mode nil))
